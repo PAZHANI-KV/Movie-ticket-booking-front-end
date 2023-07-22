16ed9c3cfc58d1356d4c646ac7edba799ada933b
PAZHANI-KV <pazhanivel11111@gmail.com>
2023-07-22 11:58:52 +0530

    c

---
+++ src/constant.js
@@ -0,0 +1,1 @@
+export const BASE_URL = "https://ticket-booking-moc1.onrender.com/"
---
+++ src/miniComponents/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <h1>Book<span> M</span>y<span> T</span>ickets </h1>
+       <p>Design & Built by Pazhani</p>
+       <p>&copy; Pazhani 2023</p>
+    </div>
+  )
+}
